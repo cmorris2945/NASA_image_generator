6bd25a84887ec9c6866ca9db4f3a96f5e346c47d
cmorris2945 <cmorris2945@yahoo.com>
2023-09-20 03:46:39 -0400

    created api and run program

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import streamlit as st
+import os
+
+## Prepare API key and API url....
+api_key = "9Sb99fONjcsHZ7lUYQcKJUBEJrz7iDiKfnkeEWr9"
+url = "https://api.nasa.gov/planetary/apod?"f"api_key={api_key}"
+
+
+## Get the request data as a dictionary....
+
+response1 = requests.get(url)
+data = response1.json()
+
+## Extract the image title, url, explanation...
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+## Download the image...
+image_filepath = "../img.png"
+response2 = requests.get(image_url)
+with open(image_filepath, 'wb') as file:
+    file.write(response2.content)
+
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
